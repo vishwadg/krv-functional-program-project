31c5413e2d45735602976a5ffd3045597527ec0c
Rahul Niraula <rahul.niraula@gmail.com>
2022-07-07 16:35:45 -0500

    minor

--- src/utils/FunctionUtils.java
+++ src/utils/FunctionUtils.java
@@ -1,4 +1,14 @@
 package utils;
 
+import model.Marketplace;
+import model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class FunctionUtils {
+    static TriFunction<Marketplace,Long,Integer, Optional<List<User>>> d=(marketplace,k,year)->
+         Stream.of(marketplace).flatMap(m->m);
+    ;
 }
