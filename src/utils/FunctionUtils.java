5fc41f1a21f3102049e1e6583a4a828a7bd86bc3
crisnaxtha <krishnashrestha49@gmail.com>
2022-07-07 17:37:54 -0500

    Worked on Get K Top users with max Comments

--- src/utils/FunctionUtils.java
+++ src/utils/FunctionUtils.java
@@ -5,12 +5,21 @@ import model.User;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FunctionUtils {
-    static TriFunction<Marketplace,Long,Integer, Optional<List<User>>> d=(marketplace,k,year)->
-         Stream.of(marketplace).flatMap(m->m.getProducts().stream())
+    static TriFunction<Marketplace,Long,Integer, Optional<List<User>>> getKTopUserWithMaxComments=(marketplace,k,year)->
+         Optional.of(Stream.of(marketplace)
+                 .flatMap(m->m.getProducts().stream())
                  .flatMap(p->p.getCommenters().stream())
-                 .flatMap();
-    ;
+                 .flatMap(c->c.getComments().stream())
+                 .filter(d->d.getCreatedAt().getYear() == year)
+                 .collect(Collectors.groupingBy(u -> u.getUser()))
+                 .entrySet()
+                 .stream().sorted((c1, c2)->(int)(c2.getValue().stream().count() - c1.getValue().stream().count()))
+                 .limit(k)
+                 .map(e ->e.getKey())
+                 .collect(Collectors.toList()));
+
 }
