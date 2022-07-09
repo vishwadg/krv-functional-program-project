8d02735f5a8352845c82d3368681dca8a5a33318
vishwa <vishwadeep.ghimire@gmail.com>
2022-07-08 19:01:09 -0500

    Minor refactor

--- src/utils/FunctionUtils.java
+++ src/utils/FunctionUtils.java
@@ -58,19 +58,6 @@ public class FunctionUtils {
 
 
     //    =====================================================================================================
-    //14. Top K users who uploaded negotiable product with highest comments
-
-    public static BiFunction<Marketplace,Integer,Map<User,List<Comment>>> usersWithHighestComments=(marketplace,year)->
-            Stream.of(marketplace)
-                    .flatMap(m->m.getProducts()!=null?m.getProducts().stream():Stream.empty())
-                    .filter(p->p.isNegotiable())
-                    .flatMap(p->p.getComments()!=null?p.getComments().stream():Stream.empty())
-                    .collect(Collectors.groupingBy(c->c.getUser()))
-                    .entrySet()
-                    .stream()
-//                    .sorted((e1,e2)->(int)(e2.getValue().stream().count()-e1.getValue().stream().count()))
-                    .collect(Collectors.toMap(a->a.getKey(),b->b.getValue()));
-
 
     //    4.Most top K expensive biddable product added
     public static TriFunction<Marketplace, Integer, Integer, Optional<List<Product>>> getKTopExpensiveBiddableProduct = (market, k, year) ->
@@ -82,4 +69,18 @@ public class FunctionUtils {
                     .limit(k)
                     .collect(Collectors.toList())
             );
+
+    //14. Top K users who uploaded negotiable product with highest comments
+
+    public static BiFunction<Marketplace, Integer, Map<User, List<Comment>>> usersWithHighestComments = (marketplace, year) ->
+            Stream.of(marketplace)
+                    .flatMap(m -> m.getProducts() != null ? m.getProducts().stream() : Stream.empty())
+                    .filter(p -> p.isNegotiable())
+                    .flatMap(p -> p.getComments() != null ? p.getComments().stream() : Stream.empty())
+                    .collect(Collectors.groupingBy(c -> c.getUser()))
+                    .entrySet()
+                    .stream()
+//                    .sorted((e1,e2)->(int)(e2.getValue().stream().count()-e1.getValue().stream().count()))
+                    .collect(Collectors.toMap(a -> a.getKey(), b -> b.getValue()));
+
 }
